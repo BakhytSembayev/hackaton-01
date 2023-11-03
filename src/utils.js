126c03a8781975fe794b41b842f71d6bbd721037
Holyhelper1 <122649498+Holyhelper1@users.noreply.github.com>
2023-11-03 22:46:33 +0300

    Random sound (#1)
    1. Добавлен модуль `sound`
    2. Исправлен конфинг webpack
    3. Добавлены утилиты в `utils.js`
    
    ----------------------------------------------------------------
    
    Co-authored-by: argon <argon143@mail.ru>
    Co-authored-by: matadogasu <github@matadogasu.xyz>

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,14 @@
-export function random(min, max) {
+export function randomNumber(min, max) {
   return Math.round(min - 0.5 + Math.random() * (max - min + 1))
+}
+
+export const getRandomColor = () => {
+  return `${Math.round(0xffffff * Math.random()).toString(16)}`;
+}
+
+export const importAll = (_context) => {
+  let files = {};
+  _context.keys().map((item, index) => { files[item.replace('./', '')] = _context(item); });
+  files = Object.values(files).map(file => file.replace('http://localhost:3000/', ''))
+  return files;
 }
