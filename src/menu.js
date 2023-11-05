94e83ea45d96a99d3cc6d2a78013be47e5fd6764
matadogasu <144089423+matadogasu@users.noreply.github.com>
2023-11-05 15:34:08 +0300

    Random figure
    * generateShape-1
    
    * added picture imgs
    
    * added moving animation
    
    * добавил возможность остановки и перетаскивания картинок с бум эффектом
    
    * мой background и  меню из  feature/new message
    
    * убрал свою кнопку со страницы и подцепил создание фигур на кнопку контекстного меню (случайная фигура)
    
    * Добавил дополнительный модуль canvasParticles.module.js-бегущие волны с кнопкой в контекстном меню для остановки и старта
    
    ---------
    
    Co-authored-by: Bakhyt Sembayev <bahytsembaev@gmail.com>

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,51 @@
-import { Menu } from './core/menu'
+// import {getEvent} from './utils'
+import { Menu } from './core/menu';
+import { Module } from './core/module';
 
 export class ContextMenu extends Menu {
+  constructor(selector) {
+    super(selector);
+    this.bindContextMenu();
+  }
 
+  bindContextMenu() {
+    const targetElement = document.querySelector(this.selector);
+    targetElement.addEventListener('contextmenu', event => {
+      event.preventDefault();
+      this.el.style.top = `${event.clientY}px`;
+      this.el.style.left = `${event.clientX}px`;
+      this.el.classList.add('open');
+    });
+  }
+
+  open() {
+    document.body.removeEventListener('contextmenu', this.boundOpenHandler);
+    this.boundOpenHandler = event => {
+      event.preventDefault();
+      this.el.style.top = `${event.clientY}px`;
+      this.el.style.left = `${event.clientX}px`;
+      this.el.classList.add('open');
+    };
+    document.body.addEventListener('contextmenu', this.boundOpenHandler);
+  }
+
+  close() {
+    document.body.addEventListener('click', event => {
+      event.preventDefault();
+      if (event.target.classList[0] === 'menu-item') {
+        this.el.classList.remove('open');
+      }
+      this.el.classList.remove('open');
+    });
+  }
+
+  add(array) {
+    for (let item in array) {
+      const newModule = new Module(array[item].type, array[item].text);
+      this.el.insertAdjacentHTML('beforeend', newModule.toHTML());
+    }
+  }
 }
+
+
+console.log('hallo');
